c580bd6027bf8617b93ae5623de95a9995ff28e8
frankknoll <Knoll_Frank@web.de>
2016-11-25 12:24:59 +0100

    adding tests shouldCreateSlowEyelidClosureEvent() and  shouldCreateNormalEyeBlinkEvent()

--- app/src/androidTest/java/de/antidrowsinessalarm/EventTest.java
+++ app/src/androidTest/java/de/antidrowsinessalarm/EventTest.java
@@ -3,9 +3,7 @@ package de.antidrowsinessalarm;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.Build;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -14,6 +12,7 @@ import com.google.android.gms.vision.MultiProcessor;
 import com.google.android.gms.vision.Tracker;
 import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
+import com.google.common.collect.FluentIterable;
 import com.google.common.eventbus.Subscribe;
 
 import org.junit.Before;
@@ -26,13 +25,13 @@ import java.util.List;
 import de.antidrowsinessalarm.event.Event;
 import de.antidrowsinessalarm.event.EyesClosedEvent;
 import de.antidrowsinessalarm.event.EyesOpenedEvent;
+import de.antidrowsinessalarm.event.NormalEyeBlinkEvent;
+import de.antidrowsinessalarm.event.SlowEyelidClosureEvent;
 import de.antidrowsinessalarm.eventproducer.DrowsyEventDetector;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.fail;
 
 @RunWith(AndroidJUnit4.class)
@@ -53,21 +52,6 @@ public class EventTest {
                 FaceTrackerActivity.createFaceDetector(
                         this.appContext,
                         new MultiProcessor.Builder<>(this.createFactory()).build());
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.GINGERBREAD_MR1)
-    @Test
-    public void shouldCreateEyesOpenedClosedEvents() {
-        // When
-        // this.detectorConsumesVideo(30, R.raw.slow_eyelid_closure);
-
-        // Then
-        assertThat(this.listener.getEvents(), hasSize(5));
-        assertThat(this.listener.getEvents().get(0), is(instanceOf(EyesOpenedEvent.class)));
-        assertThat(this.listener.getEvents().get(1), is(instanceOf(EyesClosedEvent.class)));
-        assertThat(this.listener.getEvents().get(2), is(instanceOf(EyesOpenedEvent.class)));
-        assertThat(this.listener.getEvents().get(3), is(instanceOf(EyesClosedEvent.class)));
-        assertThat(this.listener.getEvents().get(4), is(instanceOf(EyesOpenedEvent.class)));
     }
 
     @Test
@@ -97,7 +81,9 @@ public class EventTest {
         // Then
         assertThat(
                 this.listener.getEvents(),
-                contains(instanceOf(EyesOpenedEvent.class), instanceOf(EyesClosedEvent.class)));
+                contains(
+                        instanceOf(EyesOpenedEvent.class),
+                        instanceOf(EyesClosedEvent.class)));
     }
 
     @Test
@@ -110,7 +96,34 @@ public class EventTest {
         // Then
         assertThat(
                 this.listener.getEvents(),
-                contains(instanceOf(EyesOpenedEvent.class), instanceOf(EyesClosedEvent.class), instanceOf(EyesOpenedEvent.class)));
+                contains(
+                        instanceOf(EyesOpenedEvent.class),
+                        instanceOf(EyesClosedEvent.class),
+                        instanceOf(EyesOpenedEvent.class)));
+    }
+
+    @Test
+    public void shouldCreateSlowEyelidClosureEvent() {
+        // When
+        this.detectorConsumesImage(R.drawable.eyes_closed, 0);
+        this.detectorConsumesImage(R.drawable.eyes_opened, 501);
+
+        // Then
+        assertThat(this.filterEvents(SlowEyelidClosureEvent.class), contains(instanceOf(SlowEyelidClosureEvent.class)));
+    }
+
+    @Test
+    public void shouldCreateNormalEyeBlinkEvent() {
+        // When
+        this.detectorConsumesImage(R.drawable.eyes_closed, 0);
+        this.detectorConsumesImage(R.drawable.eyes_opened, 499);
+
+        // Then
+        assertThat(this.filterEvents(NormalEyeBlinkEvent.class), contains(instanceOf(NormalEyeBlinkEvent.class)));
+    }
+
+    private <T> List<T> filterEvents(final Class<T> eventFilter) {
+        return FluentIterable.from(this.listener.getEvents()).filter(eventFilter).toList();
     }
 
     private void detectorConsumesImage(final int imageResource, final int millis) {
@@ -156,7 +169,7 @@ public class EventTest {
 
         @Subscribe
         public void recordEvent(final Event event) {
-            if(event instanceof EyesOpenedEvent || event instanceof EyesClosedEvent) {
+            if(event instanceof EyesOpenedEvent || event instanceof EyesClosedEvent || event instanceof SlowEyelidClosureEvent || event instanceof NormalEyeBlinkEvent) {
                 this.events.add(event);
             }
         }
