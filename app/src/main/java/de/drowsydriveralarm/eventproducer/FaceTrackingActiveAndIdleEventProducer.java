5d34c78a17d792e7214e6426edc6e01c7b942977
frankknoll <Knoll_Frank@web.de>
2017-04-14 01:45:23 +0200

    refactoring

--- app/src/main/java/de/drowsydriveralarm/eventproducer/FaceTrackingActiveAndIdleEventProducer.java
+++ app/src/main/java/de/drowsydriveralarm/eventproducer/FaceTrackingActiveAndIdleEventProducer.java
@@ -1,9 +1,13 @@
 package de.drowsydriveralarm.eventproducer;
 
+import android.support.annotation.NonNull;
+
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.Tracker;
 import com.google.android.gms.vision.face.Face;
 import com.google.common.eventbus.EventBus;
+
+import org.joda.time.Instant;
 
 import de.drowsydriveralarm.Clock;
 import de.drowsydriveralarm.event.AppActiveEvent;
@@ -22,33 +26,62 @@ public class FaceTrackingActiveAndIdleEventProducer extends Tracker<Face> {
 
     @Override
     public void onNewItem(final int i, final Face face) {
-        if(this.activeState.isUnknown() || this.activeState.isIdle()) {
-            this.activeState.setActive();
-            this.eventBus.post(new AppActiveEvent(this.clock.now()));
-        }
+        this.onTransitionFromUnknownOrIdle2Active(this.createAppActiveEvent(this.clock.now()));
     }
 
     @Override
     public void onUpdate(final Detector.Detections<Face> detections, final Face face) {
-        if (this.activeState.isUnknown() || this.activeState.isIdle()) {
-            this.activeState.setActive();
-            this.eventBus.post(new AppActiveEvent(this.clock.now()));
-        }
+        this.onTransitionFromUnknownOrIdle2Active(this.createAppActiveEvent(this.getInstant(detections)));
     }
 
     @Override
     public void onMissing(final Detector.Detections<Face> detections) {
-        if (this.activeState.isUnknown() || this.activeState.isActive()) {
-            this.activeState.setIdle();
-            this.eventBus.post(new AppIdleEvent(this.clock.now()));
-        }
+        this.onTransitionFromUnknownOrActive2Idle(this.createAppIdleEvent(this.getInstant(detections)));
     }
 
     @Override
     public void onDone() {
+        this.onTransitionFromUnknownOrActive2Idle(this.createAppIdleEvent(this.clock.now()));
+    }
+
+    private void onTransitionFromUnknownOrIdle2Active(final Runnable runnable) {
+        if(this.activeState.isUnknown() || this.activeState.isIdle()) {
+            this.activeState.setActive();
+            runnable.run();
+        }
+    }
+
+    private void onTransitionFromUnknownOrActive2Idle(final Runnable runnable) {
         if (this.activeState.isUnknown() || this.activeState.isActive()) {
             this.activeState.setIdle();
-            this.eventBus.post(new AppIdleEvent(this.clock.now()));
+            runnable.run();
         }
+    }
+
+    @NonNull
+    private Runnable createAppActiveEvent(final Instant instant) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                FaceTrackingActiveAndIdleEventProducer.this.eventBus.post(
+                        new AppActiveEvent(instant));
+            }
+        };
+    }
+
+    @NonNull
+    private Runnable createAppIdleEvent(final Instant instant) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                FaceTrackingActiveAndIdleEventProducer.this.eventBus.post(
+                        new AppIdleEvent(instant));
+            }
+        };
+    }
+
+    @NonNull
+    private Instant getInstant(Detector.Detections<Face> detections) {
+        return new Instant(detections.getFrameMetadata().getTimestampMillis());
     }
 }
