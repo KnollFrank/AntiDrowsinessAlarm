3ee629811d6f51491658edc2a08a6c9367c6a164
frankknoll <Knoll_Frank@web.de>
2017-04-14 17:03:54 +0200

    refactoring

--- app/src/main/java/de/drowsydriveralarm/eventproducer/EventSubscriberProvider.java
+++ app/src/main/java/de/drowsydriveralarm/eventproducer/EventSubscriberProvider.java
@@ -7,28 +7,21 @@ import java.util.Collection;
 
 class EventSubscriberProvider {
 
-    private final EventBus eventBus;
-    private final IDrowsyEventDetectorConfig config;
     private final Collection<Object> eventSubscribers;
     private final SlowEyelidClosureEventsProvider slowEyelidClosureEventsProvider;
 
     public EventSubscriberProvider(final EventBus eventBus, final IDrowsyEventDetectorConfig config) {
-        this.eventBus = eventBus;
-        this.config = config;
-        this.slowEyelidClosureEventsProvider = new SlowEyelidClosureEventsProvider(this.config.getTimeWindow());
-        this.eventSubscribers = this.createEventSubscribers(this.slowEyelidClosureEventsProvider);
-    }
-
-    private Collection<Object> createEventSubscribers(final SlowEyelidClosureEventsProvider slowEyelidClosureEventsProvider) {
-        return ImmutableList.
-                builder()
-                .add(new EyesOpenedEventProducer(this.config.getEyeOpenProbabilityThreshold(), this.eventBus))
-                .add(new EyesClosedEventProducer(this.config.getEyeOpenProbabilityThreshold(), this.eventBus))
-                .add(new NormalEyeBlinkEventProducer(this.config.getSlowEyelidClosureMinDuration(), this.eventBus))
-                .add(new SlowEyelidClosureEventProducer(this.config.getSlowEyelidClosureMinDuration(), this.eventBus))
-                .add(new PendingSlowEyelidClosureEventProducer(this.config.getEyeOpenProbabilityThreshold(), this.config.getSlowEyelidClosureMinDuration(), this.eventBus))
-                .add(slowEyelidClosureEventsProvider)
-                .build();
+        this.slowEyelidClosureEventsProvider = new SlowEyelidClosureEventsProvider(config.getTimeWindow());
+        this.eventSubscribers =
+                ImmutableList.
+                        builder()
+                        .add(new EyesOpenedEventProducer(config.getEyeOpenProbabilityThreshold(), eventBus))
+                        .add(new EyesClosedEventProducer(config.getEyeOpenProbabilityThreshold(), eventBus))
+                        .add(new NormalEyeBlinkEventProducer(config.getSlowEyelidClosureMinDuration(), eventBus))
+                        .add(new SlowEyelidClosureEventProducer(config.getSlowEyelidClosureMinDuration(), eventBus))
+                        .add(new PendingSlowEyelidClosureEventProducer(config.getEyeOpenProbabilityThreshold(), config.getSlowEyelidClosureMinDuration(), eventBus))
+                        .add(this.slowEyelidClosureEventsProvider)
+                        .build();
     }
 
     public Collection<Object> getEventSubscribers() {
