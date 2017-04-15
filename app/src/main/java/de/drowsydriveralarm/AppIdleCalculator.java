18789e921f4e3a704b45457d645035c273d971b6
frankknoll <Knoll_Frank@web.de>
2017-04-15 16:42:36 +0200

    refactoring

--- app/src/main/java/de/drowsydriveralarm/AppIdleCalculator.java
+++ app/src/main/java/de/drowsydriveralarm/AppIdleCalculator.java
@@ -1,5 +1,6 @@
 package de.drowsydriveralarm;
 
+import com.google.common.base.Optional;
 import com.google.common.eventbus.Subscribe;
 
 import org.joda.time.Duration;
@@ -11,25 +12,25 @@ import de.drowsydriveralarm.event.AppIdleEvent;
 class AppIdleCalculator {
 
     private Duration appIdleDuration = new Duration(0);
-    private AppIdleEvent appIdleEvent;
+    private Optional<AppIdleEvent> idleEventBeforeActiveEvent = Optional.absent();
 
     public AppIdleCalculator() {
     }
 
     @Subscribe
     public void onAppIdle(final AppIdleEvent appIdleEvent) {
-        this.appIdleEvent = appIdleEvent;
+        this.idleEventBeforeActiveEvent = Optional.of(appIdleEvent);
     }
 
     @Subscribe
     public void onAppActive(final AppActiveEvent appActiveEvent) {
-        this.appIdleDuration = this.appIdleDuration.plus(new Duration(this.appIdleEvent.getInstant(), appActiveEvent.getInstant()));
-        this.appIdleEvent = null;
+        this.appIdleDuration = this.appIdleDuration.plus(new Duration(this.idleEventBeforeActiveEvent.get().getInstant(), appActiveEvent.getInstant()));
+        this.idleEventBeforeActiveEvent = Optional.absent();
     }
 
     public Duration getAppIdleDuration(final Instant now) {
-        if (this.appIdleEvent != null) {
-            return this.appIdleDuration.plus(new Duration(this.appIdleEvent.getInstant(), now));
+        if (this.idleEventBeforeActiveEvent.isPresent()) {
+            return this.appIdleDuration.plus(new Duration(this.idleEventBeforeActiveEvent.get().getInstant(), now));
         } else {
             return this.appIdleDuration;
         }
